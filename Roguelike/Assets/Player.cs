d0e3dd071d94ed1d377405926e2e18a446fbbd4a
Hao <54834780+HaoYeChen@users.noreply.github.com>
2020-02-17 14:56:11 +0100

    movement script update

--- Roguelike/Assets/Player.cs
+++ Roguelike/Assets/Player.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //movement & jump
+    private float movementInputDirection;
     public float movementSpeed = 5f;
+    private bool isFacingRight = true;
+    public float jumpForce = 30f;
 
+    // groundcheck
     private float moveInput;
     private bool isGrounded;
     public Transform groundCheck;
@@ -24,14 +29,40 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-
+        CheckInput();
+        CheckMovementDirection();
     }
 
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        ApplyMovement();
+    }
 
-        moveInput = Input.GetAxis("Horizontal");
-        playerRigidbody.velocity = new Vector2(moveInput * movementSpeed, playerRigidbody.velocity.y);
+    private void CheckMovementDirection()
+    {
+        if (isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void ApplyMovement()
+    {
+        playerRigidbody.velocity = new Vector2(movementSpeed * movementInputDirection, playerRigidbody.velocity.y);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
